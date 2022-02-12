b733c503c2028b4a574563203cebcd7bad71a8dc
Schallom Awolou <64165396+Schlomosys@users.noreply.github.com>
2022-02-12 21:14:41 +0100

    Update settings.py

--- nassirath/settings.py
+++ nassirath/settings.py
@@ -28,7 +28,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT='https://nassirathsbirthday.herokuapp.com/media/'
 MEDIA_URL = '/media/'
 SECRET_KEY =os.environ['SECRET_KEY']
 DEBUG =True
@@ -126,7 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
